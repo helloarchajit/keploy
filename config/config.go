30c991e33ebac4b4deea720b35002074d6bb4d95
Anju Pathak <anjupathak9810@gmail.com>
2025-12-01 05:12:05 +0000

    feat: added module level debug logs
    Signed-off-by: Anju Pathak <anjupathak9810@gmail.com>

--- config/config.go
+++ config/config.go
@@ -9,6 +9,12 @@ import (
 	"go.keploy.io/server/v3/pkg/models"
 )
 
+// Add after line 10 (imports section)
+type DebugConfig struct {
+    Enabled bool            `json:"enabled" yaml:"enabled" mapstructure:"enabled"`
+    Modules map[string]bool `json:"modules" yaml:"modules" mapstructure:"modules"`
+}
+
 type MockDownload struct {
 	RegistryIDs []string `json:"registryIds" yaml:"registryIds" mapstructure:"registryIds"`
 }
@@ -24,6 +30,7 @@ type Config struct {
 	DNSPort               uint32              `json:"dnsPort" yaml:"dnsPort" mapstructure:"dnsPort"`
 	ProxyPort             uint32              `json:"proxyPort" yaml:"proxyPort" mapstructure:"proxyPort"`
 	Debug                 bool                `json:"debug" yaml:"debug" mapstructure:"debug"`
+	DebugConfig			  DebugConfig `json:"debugConfig" yaml:"debugConfig" mapstructure:"debugConfig"` 
 	DisableTele           bool                `json:"disableTele" yaml:"disableTele" mapstructure:"disableTele"`
 	DisableANSI           bool                `json:"disableANSI" yaml:"disableANSI" mapstructure:"disableANSI"`
 	InDocker              bool                `json:"inDocker" yaml:"-" mapstructure:"inDocker"`
